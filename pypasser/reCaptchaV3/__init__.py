587385771b123c0690f2f22e9938084f3c15e031
ProtDos <69071809+ProtDos@users.noreply.github.com>
2025-03-22 19:09:34 +0100

    Update __init__.py

--- pypasser/reCaptchaV3/__init__.py
+++ pypasser/reCaptchaV3/__init__.py
@@ -1,10 +1,11 @@
 from pypasser.exceptions import RecaptchaTokenNotFound, RecaptchaResponseNotFound
-from pypasser.session import Session
+from pypasser.session import Session, AsyncSession
 from pypasser.structs import Proxy
 from pypasser.utils import parse_url
 from .constants import POST_DATA, BASE_URL, BASE_HEADERS
-
 import re
+import aiohttp
+import asyncio
 from typing import Dict, Union
 
 class reCaptchaV3:
@@ -86,3 +87,68 @@ class reCaptchaV3:
             raise RecaptchaResponseNotFound()
         
         return results[0]
+
+
+class AsyncReCaptchaV3:
+    """
+    Asynchronous reCaptchaV3 bypass using aiohttp.
+    """
+
+    def __init__(
+            self,
+            anchor_url: str,
+            proxy: Union[Proxy, Dict] = None,
+            timeout: Union[int, float] = 20,
+    ):
+        self.anchor_url = anchor_url
+        self.proxy = proxy
+        self.timeout = timeout
+        self.prefix = "https://www.google.com/recaptcha/"
+
+    async def solve(self) -> str:
+        """
+        Solves the reCaptcha v3 asynchronously.
+        """
+        data = parse_url(self.anchor_url)
+
+        async with aiohttp.ClientSession(headers=BASE_HEADERS) as session:
+            # Gets recaptcha token.
+            token = await self.get_recaptcha_token(session, data["endpoint"], data["params"])
+
+            params = dict(pair.split("=") for pair in data["params"].split("&"))
+
+            # Gets recaptcha response.
+            post_data = POST_DATA.format(params["v"], token, params["k"], params["co"])
+
+            recaptcha_response = await self.get_recaptcha_response(session, data["endpoint"], f'k={params["k"]}',
+                                                                   post_data)
+
+        return recaptcha_response
+
+    async def get_recaptcha_token(self, session: aiohttp.ClientSession, endpoint: str, params: str) -> str:
+        """
+        Sends GET request to `anchor URL` to get recaptcha token asynchronously.
+        """
+        async with session.get(f"{self.prefix}{endpoint}/anchor", params=params) as response:
+            response_text = await response.text()
+
+        results = re.findall(r'"recaptcha-token" value="(.*?)"', response_text)
+        if not results:
+            raise RecaptchaTokenNotFound()
+
+        return results[0]
+
+    async def get_recaptcha_response(self, session: aiohttp.ClientSession, endpoint: str, params: str,
+                                     data: str) -> str:
+        """
+        Sends POST request to `reload URL` to get recaptcha response asynchronously.
+        """
+        async with session.post(f"{self.prefix}{endpoint}/reload", params=params, data=data) as response:
+            response_text = await response.text()
+
+        results = re.findall(r'"rresp","(.*?)"', response_text)
+        if not results:
+            raise RecaptchaResponseNotFound()
+
+        return results[0]
+
